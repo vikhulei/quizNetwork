1c87d6a214eefdc963f423cdeb2f3329c3309a22
vikhulei <65869915+vikhulei@users.noreply.github.com>
2022-12-07 17:42:19 +0000

    Update Questions.js

--- src/Questions.js
+++ src/Questions.js
@@ -1,7 +1,12 @@
 const Questions = {
   js_basics: [
     { q: 'Comments in javascript', a: 'one line - //   multiple lines - /*  */', tag: 0 },
-    { q: 'What is a varible?', a: 'Variable is container for a value', tag: 0 },
+    { q: '', a: '', tag: 0 },
+    { q: 'What is a DOM?', a: 'Document Object Model, it is a structural representation of the document', tag: 0 },
+    { q: 'What is ECMA?', a: 'European Association for standartizing information. ECMA Script is standartized JavaScript', tag: 0 },
+    { q: 'Where JavaScrip has borrowed its syntax from?', a: 'Java, C, C++', tag: 0 },
+    { q: 'What does it mean just-in-time compilation', a: 'Compilation during execution of a program', tag: 0 },
+    { q: 'What is a varible?', a: 'Variable is an address/name to a storage location in memory', tag: 0 },
     { q: 'Var, let and const', a: "VAR: function scoped, can be re-declared in the same scope ------- LET: block & function scoped, cannot be re-declared in the same scope ----------------- CONST: block scoped, cannot be updated, not initialized", tag: 0 },
     { q: 'Complex operators', a: '**, %,  ++, --, +=, -=, *=, /=, %=, **=', tag: 0 },
     { q: 'Explain this keyword', a: 'Refers to the object it belongs to. If called with argument from another object, will refer to that other object.  In arrow function will always refer to the object which owns the function.', tag: 0 },
