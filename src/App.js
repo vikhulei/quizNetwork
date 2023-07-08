26326fe25c12abb42002d1afc8d90314cc52f301
vikhulei <65869915+vikhulei@users.noreply.github.com>
2023-07-08 21:33:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Questions from "./Questions";
 const App = () => {
   const [question, setQuestion] = useState({
     q: "Start",
-    a: "<em>Press</em> Next",
+    a: "Press Next",
     tag: 0
   });
   const [category, setCategory] = useState("js_basics");
