b3f07a21fa800c3755242cbc69eb33789e0f6886
vikhulei <vikhulei@gmail.com>
2023-07-09 22:13:25 +0100

    questions added

--- src/Questions.js
+++ src/Questions.js
@@ -1,6 +1,14 @@
 const Questions = {
   network: [
     { q: 'What are common methods of data transmission?', a: '1. Electrical signals<br>2. Optical signals<br>3. Wireless signals', tag: 0 },
+    { q: 'Name network types', a: '1. Small home<br>2. Small office/home office<br>3. Medium to large<br>4. Internet', tag: 0 },
+    { q: 'Types of personal data', a: '1. Volunteer data<br>2. Observed data<br>3. Inferred data', tag: 0 },
+    { q: 'What is P2P network?', a: 'Two computers connected directly, both are client and server at the same time', tag: 0 },
+    { q: 'What is IP phone?', a: 'It uses VoIP technology. Can be hardware phone or softphone', tag: 0 },
+    { q: '', a: '', tag: 0 },
+    { q: '', a: '', tag: 0 },
+    { q: '', a: '', tag: 0 },
+
     ],
   
 };
