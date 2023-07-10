4aba71c339e7346afb9a77f65e28d867ae5a25d3
vikhulei <vikhulei@gmail.com>
2023-07-10 20:07:01 +0100

    questions added

--- src/Questions.js
+++ src/Questions.js
@@ -5,9 +5,12 @@ const Questions = {
     { q: 'Types of personal data', a: '1. Volunteer data<br>2. Observed data<br>3. Inferred data', tag: 0 },
     { q: 'What is P2P network?', a: 'Two computers connected directly, both are client and server at the same time', tag: 0 },
     { q: 'What is IP phone?', a: 'It uses VoIP technology. Can be hardware phone or softphone', tag: 0 },
-    { q: '', a: '', tag: 0 },
-    { q: '', a: '', tag: 0 },
-    { q: '', a: '', tag: 0 },
+    { q: 'Two options to connect home user to ISP. Explain each', a: '1) Coaxial cable<br>2. DSL - Digital Subscriber Line', tag: 0 },
+    { q: 'What is SSID?', a: 'Service Set Identifier<br>It Uniquely names a WIFI network', tag: 0 },
+    { q: 'What is GSM?', a: 'Global System for Mobile Communications', tag: 0 },
+    { q: 'Whats is NFC?', a: 'Near Field Communications', tag: 0 },
+    { q: 'Distances covered by 1) Wifi, 2) Bluetooth 3) WWAN', a: '1) Wifi - 0.18 miles<br>2) Bluetooth - 0.05mi<br>3) WWAN (Wireless wide-area network) - few miles', tag: 0 },
+    { q: 'Which 2 technologies are used in tethering?', a: '1) USB<br>2) Bluetooth', tag: 0 },
 
     ],
   
